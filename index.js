7e35a3a930e76f182c197241b3036f89ebe7c8fb
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-26 14:01:33 -0700

    Do not fail

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ module.exports = function eachPackage(command, options, callback) {
     options = {};
   }
   options = options || {};
-
   var iterator = new Iterator(process.cwd(), {
     filter: function filter(entry) {
       if (entry.stats.isDirectory()) return entry.basename !== '.git' && entry.basename !== 'node_modules';
@@ -27,14 +26,41 @@ module.exports = function eachPackage(command, options, callback) {
     },
     alwaysStat: true,
   });
+
+  var counter = 0;
+  var errors = [];
   iterator.forEach(
     function (entry, callback) {
       if (!entry.stats.isFile()) return callback();
+      counter++;
       execCommand(command, { relativePath: path.dirname(entry.path), cwd: path.dirname(entry.fullPath), silent: options.silent }, function (err) {
-        callback(err);
+        if (err) errors.push({ command: command, entry: entry, error: err });
+        callback();
       });
     },
     { callbacks: true, concurrency: 1 },
-    callback
+    function iteratorCallback(err) {
+      if (err) return callback(err);
+      if (!errors.length) {
+        if (!options.silent) {
+          console.log('\n**********************');
+          console.log('Success (' + counter + ')');
+          console.log('**********************');
+        }
+
+        return callback();
+      }
+
+      if (!options.silent) {
+        console.log('\n**********************');
+        console.log('Errors (' + errors.length + ' of ' + counter + ')');
+        for (var i = 0; i < errors.length; i++) {
+          var error = errors[i];
+          console.log(error.entry.path, error.error.message);
+        }
+        console.log('**********************');
+      }
+      callback(new Error('Errors ' + errors.length));
+    }
   );
 };
