b2a3c1d4e9655ff7852ee5671026ef11ebef2859
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-01 11:38:36 -0800

    Migrated to typescript

--- bin/cli.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-require('../dist/cjs/cli.js')(process.argv.slice(2));
--- test/spec/cli.test.ts
+++ test/spec/cli.test.ts
@@ -1,13 +1,12 @@
-// remove NODE_OPTIONS from ts-dev-stack
-delete process.env.NODE_OPTIONS;
+import assert from 'assert';
+import path from 'path';
+import url from 'url';
+import cr from 'cr';
+import spawn from 'cross-spawn-cb';
+import isVersion from 'is-version';
 
-const assert = require('assert');
-const path = require('path');
-const spawn = require('cross-spawn-cb');
-const isVersion = require('is-version');
-const cr = require('cr');
-
-const CLI = path.join(__dirname, '..', '..', 'bin', 'cli.js');
+const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
+const CLI = path.join(__dirname, '..', '..', 'bin', 'cli.cjs');
 
 describe('cli', () => {
   describe('happy path', () => {
