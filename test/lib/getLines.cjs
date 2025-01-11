5f0542cd792e021480f1f04e7dbe3400bf65cf12
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-11 13:27:53 -0800

    Update getLines to split on colon followed by space

--- test/lib/getLines.cjs
+++ test/lib/getLines.cjs
@@ -3,6 +3,6 @@ const cr = require('cr');
 module.exports = function getLines(stdout) {
   return cr(stdout)
     .split('\n')
-    .map((line) => (line.indexOf(':') >= 0 ? line.split(':')[1] : line))
+    .map((line) => (line.indexOf(': ') >= 0 ? line.split(': ')[1] : line))
     .filter((line) => line.length > 0);
 };
