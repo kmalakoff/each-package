044fb8fb7e59a0c21e785d8dfa47162aa16d6d1e
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-11 11:44:09 -0800

    Upgrade modules

--- src/worker.ts
+++ src/worker.ts
@@ -1,15 +1,19 @@
+import fs from 'fs';
 import path from 'path';
-import spawn, { type SpawnResult } from 'cross-spawn-cb';
+import type { RawManifest } from '@lerna-lite/core';
 import Iterator from 'fs-iterator';
-import spawnStreaming from 'spawn-streaming';
-const THROTTLE_DURATION = 3000; // 3 sec
+import removeBOM from 'remove-bom-buffer';
+import spawnStreaming, { type SpawnResult } from 'spawn-streaming';
+// @ts-ignore
+import { Package } from '../../assets/package.cjs';
+// @ts-ignore
+import { runTopologically } from '../../assets/run-topologically.cjs';
 
 export default function each(command, args, options, callback) {
   let depth = typeof options.depth === 'undefined' ? Infinity : options.depth;
   if (depth !== Infinity) depth++; // depth is relative to first level of packages
   const concurrency = typeof options.concurrency === 'undefined' ? 1 : options.concurrency;
   const cwd = typeof options.cwd === 'undefined' ? process.cwd() : options.cwd;
-  const inherit = options.stdout === 'inherit' || options.stdio === 'inherit';
 
   const iterator = new Iterator(cwd, {
     filter: function filter(entry) {
@@ -19,37 +23,42 @@ export default function each(command, args, options, callback) {
     depth,
   });
 
-  const results = [];
+  const packages = [];
   iterator.forEach(
-    (entry, callback) => {
-      if (!entry.stats.isFile()) return callback();
-
-      const spawnOptions = { ...options, cwd: path.dirname(entry.fullPath) };
-
-      if (!inherit || concurrency < 2) {
-        spawn(command, args, spawnOptions, (err, res) => {
-          if (err && err.message.indexOf('ExperimentalWarning') >= 0) {
-            res = err as unknown as SpawnResult;
-            err = null;
-          }
-          results.push({ path: path.dirname(entry.path), error: err, result: res });
-          callback();
-        });
-      } else {
-        spawnStreaming(command, args, spawnOptions, { prefix: path.dirname(entry.path), throttle: THROTTLE_DURATION }, (err, res) => {
-          if (err && err.message.indexOf('ExperimentalWarning') >= 0) {
-            res = err as unknown as SpawnResult;
-            err = null;
-          }
-          results.push({ path: path.dirname(entry.path), error: err, result: res });
-          callback();
-        });
-      }
+    (entry) => {
+      if (!entry.stats.isFile()) return;
+      const pkg = JSON.parse(removeBOM(fs.readFileSync(entry.fullPath)).toString());
+      packages.push(new Package({ ...pkg, devDependencies: {} } as unknown as RawManifest, path.dirname(entry.fullPath), cwd));
     },
-    { callbacks: true, concurrency },
-    (err) => {
-      if (err) err.results = results;
-      err ? callback(err) : callback(null, results);
+    async (err) => {
+      if (err) return callback(err);
+
+      const results = [];
+      try {
+        await runTopologically(
+          packages,
+          function runner(pkg) {
+            return new Promise((resolve, reject) => {
+              spawnStreaming(command, args, { ...options, cwd: pkg.location }, { prefix: pkg.name }, (err, res) => {
+                if (err && err.message.indexOf('ExperimentalWarning') >= 0) {
+                  res = err as unknown as SpawnResult;
+                  err = null;
+                }
+                results.push({ path: path.dirname(pkg.location), error: err, result: res });
+                err ? reject(err) : resolve(undefined);
+              });
+            });
+          },
+          {
+            concurrency,
+            rejectCycles: false,
+          }
+        );
+        return callback(null, results);
+      } catch (err) {
+        err.results = results;
+        return callback(err);
+      }
     }
   );
 }
