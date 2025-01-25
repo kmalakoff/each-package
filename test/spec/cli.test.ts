ac0ec3839fa5f92918fbf5e75e31277a532d976c
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-25 10:51:45 -0800

    Add expanded option and integrate spawn-term for command execution

--- test/spec/cli.test.ts
+++ test/spec/cli.test.ts
@@ -1,9 +1,8 @@
 import assert from 'assert';
 import path from 'path';
 import url from 'url';
+import cr from 'cr';
 import spawn from 'cross-spawn-cb';
-import isVersion from 'is-version';
-import getLines from '../lib/getLines.cjs';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 const CLI = path.join(__dirname, '..', '..', 'bin', 'cli.cjs');
@@ -15,25 +14,30 @@ const NODE = isWindows ? 'node.exe' : 'node';
 describe('cli', () => {
   describe('happy path', () => {
     it('basic command', (done) => {
+      const expected = '"hello"';
       spawn(CLI, ['--silent', 'echo', '"hello"'], { encoding: 'utf8', cwd: NODE_MODULES }, (err, res) => {
         if (err) return done(err.message);
-        assert.equal(getLines(res.stdout).slice(-2, -1)[0], '"hello"');
+        assert.ok(res.stdout.indexOf(expected) >= 0);
         done();
       });
     });
 
     it('basic command with options', (done) => {
+      const res = spawn.sync(NODE, ['--version'], { encoding: 'utf8' });
+      const expected = cr(res.stdout).split('\n')[0];
       spawn(CLI, ['--silent', NODE, '--version'], { encoding: 'utf8', cwd: NODE_MODULES }, (err, res) => {
         if (err) return done(err.message);
-        assert.ok(isVersion(getLines(res.stdout).slice(-2, -1)[0], 'v'));
+        assert.ok(res.stdout.indexOf(expected) >= 0);
         done();
       });
     });
 
     it('basic command with options (--)', (done) => {
+      const res = spawn.sync(NODE, ['--version'], { encoding: 'utf8' });
+      const expected = cr(res.stdout).split('\n')[0];
       spawn(CLI, ['--silent', '--', NODE, '--version'], { encoding: 'utf8', cwd: NODE_MODULES }, (err, res) => {
         if (err) return done(err.message);
-        assert.ok(isVersion(getLines(res.stdout).slice(-2, -1)[0], 'v'));
+        assert.ok(res.stdout.indexOf(expected) >= 0);
         done();
       });
     });
