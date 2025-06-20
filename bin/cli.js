06b49aeb276937c2e97b6bd2e40d61d495d59364
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-19 23:03:01 -0700

    Rename CLI executable to cli.js and update bin path references

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 
+// biome-ignore lint/security/noGlobalEval: dual esm and cjs
 if (typeof require === 'undefined') eval("import('../dist/esm/cli.js').then((cli) => cli.default(process.argv.slice(2), 'ep')).catch((err) => { console.log(err); process.exit(-1); });");
 else require('../dist/cjs/cli.js')(process.argv.slice(2), 'ep');
