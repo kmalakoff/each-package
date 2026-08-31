38d48bf6dadb09704761101e7406fd628551720f
Kevin Malakoff <kmalakoff@gmail.com>
2026-08-31 00:46:58 -0700

    Remove legacy comment from existsSync in compat.ts

--- src/compat.ts
+++ src/compat.ts
@@ -1,6 +1,5 @@
 import fs from 'fs';
 
-// fs.accessSync is missing before Node 4.0, so fall back to fs.statSync
 export function existsSync(test: string): boolean {
   try {
     (fs.accessSync || fs.statSync)(test);
