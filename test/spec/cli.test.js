217869031addeb59e38fd9a6cc377076db41a3c6
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-12 10:41:55 -0700

    Implement depth option for CLI and add VSCode debugging config

--- test/spec/cli.test.js
+++ test/spec/cli.test.js
@@ -5,7 +5,7 @@ var spawn = require('cross-spawn-cb');
 describe('cli', function () {
   describe('happy path', function () {
     it('basic command', function (done) {
-      spawn(path.join(__dirname, '..', '..', 'bin', 'each-package'), ['node', '--version'], { stdio: 'inherit' }, function (err, res) {
+      spawn(path.join(__dirname, '..', '..', 'bin', 'each-package'), ['--', 'node', '--version'], { stdio: 'inherit' }, function (err, res) {
         assert.ok(!err);
         assert.equal(res.exitCode, 0);
         done();
