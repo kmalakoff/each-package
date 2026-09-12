146bbd46f5fb42d08e446477499abbb4d94a2b60
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-12 10:57:02 -0700

    Refactor CLI and update dependencies

--- test/spec/cli.test.js
+++ test/spec/cli.test.js
@@ -2,17 +2,17 @@ var assert = require('assert');
 var path = require('path');
 var spawn = require('cross-spawn-cb');
 var isVersion = require('is-version');
+var cr = require('cr');
 
-var CLI = path.join(__dirname, '..', '..', 'bin', 'each-package');
-var EOL = process.platform === 'win32' ? '\r\n' : '\n';
+var CLI = path.join(__dirname, '..', '..', 'bin', 'each-package.js');
 
 describe('cli', function () {
   describe('happy path', function () {
     it('basic command', function (done) {
       spawn(CLI, ['--silent', 'npm', '--version'], { stdout: 'string' }, function (err, res) {
         assert.ok(!err);
-        assert.equal(res.code, 0);
-        assert.ok(isVersion(res.stdout.split(EOL).slice(-2, -1)[0]));
+        var lines = cr(res.stdout).split('\n');
+        assert.ok(isVersion(lines.slice(-2, -1)[0]));
         done();
       });
     });
@@ -20,8 +20,8 @@ describe('cli', function () {
     it('basic command with options', function (done) {
       spawn(CLI, ['--silent', 'node', '--version'], { stdout: 'string' }, function (err, res) {
         assert.ok(!err);
-        assert.equal(res.code, 0);
-        assert.ok(isVersion(res.stdout.split(EOL).slice(-2, -1)[0], 'v'));
+        var lines = cr(res.stdout).split('\n');
+        assert.ok(isVersion(lines.slice(-2, -1)[0], 'v'));
         done();
       });
     });
@@ -29,8 +29,8 @@ describe('cli', function () {
     it('basic command with options (--)', function (done) {
       spawn(CLI, ['--silent', '--', 'node', '--version'], { stdout: 'string' }, function (err, res) {
         assert.ok(!err);
-        assert.equal(res.code, 0);
-        assert.ok(isVersion(res.stdout.split(EOL).slice(-2, -1)[0], 'v'));
+        var lines = cr(res.stdout).split('\n');
+        assert.ok(isVersion(lines.slice(-2, -1)[0], 'v'));
         done();
       });
     });
@@ -39,8 +39,7 @@ describe('cli', function () {
   describe('unhappy path', function () {
     it('missing command', function (done) {
       spawn(CLI, ['--silent'], { stdout: 'string' }, function (err, res) {
-        assert.ok(!err);
-        assert.ok(res.code !== 0);
+        assert.ok(!!err);
         done();
       });
     });
