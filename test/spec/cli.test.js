7b2bc1256ae87ac44207d8aa414540f5562525a6
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-12 10:44:22 -0700

    Fix depth handling and update documentation

--- test/spec/cli.test.js
+++ test/spec/cli.test.js
@@ -5,6 +5,14 @@ var spawn = require('cross-spawn-cb');
 describe('cli', function () {
   describe('happy path', function () {
     it('basic command', function (done) {
+      spawn(path.join(__dirname, '..', '..', 'bin', 'each-package'), ['npm', 'whoami'], { stdio: 'inherit' }, function (err, res) {
+        assert.ok(!err);
+        assert.equal(res.exitCode, 0);
+        done();
+      });
+    });
+
+    it('basic command with options', function (done) {
       spawn(path.join(__dirname, '..', '..', 'bin', 'each-package'), ['--', 'node', '--version'], { stdio: 'inherit' }, function (err, res) {
         assert.ok(!err);
         assert.equal(res.exitCode, 0);
