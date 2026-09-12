kmalakoff/keyv-registry

Migrated to void and simplified compat layer

src/index.ts
@@ -41,18 +41,11 @@ export default function createStore<T>(uri: string, callback: CreateStoreCallbac
 export default function createStore<T>(uri: string, options: CreateStoreOptions, callback: CreateStoreCallback<T>): void;
 export default function createStore<T>(uri: string, options?: CreateStoreOptions): Promise<Keyv<T>>;
 export default function createStore<T>(uri: string, options?: CreateStoreOptions | CreateStoreCallback<T>, callback?: CreateStoreCallback<T>): void | Promise<Keyv<T>> {
-  // Normalize arguments
-  if (typeof options === 'function') {
-    callback = options;
-    options = {};
-  }
-  options = options || {};
+  callback = typeof options === 'function' ? options : callback;
+  options = typeof options === 'function' ? {} : ((options || {}) as CreateStoreOptions);
 
-  // Callback mode
   if (typeof callback === 'function') return worker(uri, options, callback);
-
-  // Promise mode
   return new Promise((resolve, reject) => {
-    worker(uri, options as CreateStoreOptions, (err, store) => (err ? reject(err) : resolve(store as Keyv<T>)));
+    worker(uri, options, (err, store) => (err ? reject(err) : resolve(store as Keyv<T>)));
   });
 }

kmalakoff/each-package

src/index.ts
@@ -10,15 +10,12 @@ export default function eachPackage(command: string, args: string[], callback: E
 export default function eachPackage(command: string, args: string[], options: EachOptions, callback: EachCallback): void;
 
 export default function eachPackage(command: string, args: string[], options?: EachOptions | EachCallback, callback?: EachCallback): void | Promise<EachResult[]> {
-  if (typeof options === 'function') {
-    callback = options;
-    options = {};
-  }
-  options = options || {};
+  callback = typeof options === 'function' ? options : callback;
+  options = typeof options === 'function' ? {} : ((options || {}) as EachOptions);
 
   if (typeof callback === 'function') return worker(command, args, options, callback);
   return new Promise((resolve, reject) =>
-    worker(command, args, options as EachOptions, (err?: EachError, results?: EachResult[]): void => {
+    worker(command, args, options, (err?: EachError, results?: EachResult[]): void => {
       err ? reject(err) : resolve(results);
     })
   );